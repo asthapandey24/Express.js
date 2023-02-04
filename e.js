8918858fee4dd5e2a15810953b6b2afba56ef527
ThrivingDep <119737884+ThrivingDep@users.noreply.github.com>
2023-02-04 11:39:16 +0530

    Routers and filters

--- e.js
+++ e.js
@@ -1,30 +1,26 @@
 
 
 const express = require('express');
-
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
+
+
 app.use(bodyParser.urlencoded({extended: false}));
-app.use('/', (req, res, next)=>{
- // console.log('This always runs');
-  next();
-})
 
-app.use('/add-product',(req, res, next) =>{
- //console.log('in the middleware');
-      res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')      //  Allows the request to continue to the next milddleware in line
-});
-app.post('/product', (req, res, next)=>{
-  console.log(req.body);
-  res.redirect('/');
-});
 
-app.use('/',(req, res, next) =>{
-  //console.log('in another middleware');
-  res.send('<h1>Hello</h1>');
 
-})
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes );
+
+app.use('/',(req, res, next)=>{
+  res.status(404).send('<h1>Page not found</h1>')
+}) ;
 
 app.listen(3000);
+
+
+
